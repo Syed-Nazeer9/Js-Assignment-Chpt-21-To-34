76fd9d34e19e904c217ae8c147c98546674ac999
Syed-Nazeer9 <Syednazeer679@gmail.com>
2023-03-22 05:17:07 +0500

    first

---
+++ app.js
@@ -0,0 +1,105 @@
+//String Methods | JAVASCRIPT//
+//Q# 01
+// var firstName= prompt("Enter your firstName");
+// var lastName= prompt("Enter your lastName");
+// var fullName ;
+// fullName =firstName.concat(" ",lastName);
+// document.write("Hi "+fullName);
+
+//Q# 02
+// var phoneModel= prompt("Enter your Favorite Mobile Model");
+// var length= phoneModel.length;
+// document.write("My Favorite Phone is: "+phoneModel + "<br>"+ "The Length of String: " +length);
+
+//Q# 03
+// var country= "Pakistani";
+// var recive= country.indexOf( 'n' );
+// document.write("String: "+ country +"<br>"+ "Index of 'n':"+recive);
+
+//Q# 04
+// var web= "Hello World";
+// var recive= web.lastIndexOf( 'l' );
+// document.write("String: "+ web +"<br>"+ "Index of 'l':"+recive);
+
+//Q# 05
+// var country= "Pakistani";
+// var recive= country.slice( '3,5' );
+// document.write("String: "+ country +"<br>"+ "Index of 3:"+recive);
+
+//Q# 06
+// var firstName= prompt("Enter your firstName");
+// var lastName= prompt("Enter your lastName");
+// var fullName ;
+// fullName =firstName.concat(" ",lastName);
+// document.write("Hi "+fullName);
+
+//Q# 07
+// var city= "Hyderabad";
+// var recive= city.replace("Hyder" , ("Islam"));
+// document.write(("City: ")+ city +("<br>"));
+// document.write("After replacment: "+ recive);
+
+//Q# 08
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var recive= message.replaceAll("and" , "&");
+// document.write(message + "<br>");
+// document.write(recive);
+
+//Q# 09
+
+
+
+
+
+
+
+//Q# 10
+// var userInput= prompt("Enter your favurate Fruite");
+// var convert= userInput.toUpperCase();
+// document.write("User Input: "+ userInput + "<br>");
+// document.write( "Upper Case: " + convert );
+
+//Q# 11
+// var userInput= prompt("Enter your ");
+// var convert= userInput.toTitleCase();
+// document.write("User Input: " + userInput + "<br>");
+// document.write( "Title Case: " + convert);
+
+
+//Q# 12
+
+
+
+
+
+
+
+
+
+//Q# 13
+// var userName = prompt("Enter your usewName: ");
+// var inValid = true;
+// var specialSymbols = ['@','.',',','!'];
+// for (var i = 0; i < specialSymbols.length; i++) {
+//     if(userName.includes(specialSymbols[i])) {
+//         isvalid = false;
+//         break;
+//     }
+// }
+// if (isValid) {
+//     alert("Enter a Valid username with special symbols [@ . , !]");
+// }
+
+//Q# 14
+
+
+
+
+
+
+
+
+
+
+
+
